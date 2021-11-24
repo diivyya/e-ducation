b76e2c2e348c93878045e4cbc3cb215bca37cfd7
Divya Tembhurne <divyatembhurne1107@gmail.com>
2021-11-25 01:12:20 +0530

    Minor changes

--- src/views/Components/Sections/ProductSection.js
+++ src/views/Components/Sections/ProductSection.js
@@ -21,13 +21,10 @@ export default function ProductSection() {
     <div className={classes.section}>
       <GridContainer justify="center">
         <GridItem xs={12} sm={12} md={8}>
-          <h2 className={classes.title}>Let{"'"}s talk product</h2>
+          <h2 className={classes.title}>About the application</h2>
           <h5 className={classes.description}>
-            This is the paragraph where you can write more details about your
-            product. Keep you user engaged by providing meaningful information.
-            Remember that by this time, the user is curious, otherwise he wouldn
-            {"'"}t scroll to get here. Add a button if you want the user to see
-            more.
+            This app provides a digital way to students as well as teachers to keep the education going in this pandemic situation.
+            Regardless of pandemic, this application is designed to be used for all use-cases of a University on a digital basis.
           </h5>
         </GridItem>
       </GridContainer>
